72b64fdf8638f3c9683e37f270315f050241f563
eragon1248 <raksheetkota@gmail.com>
2023-12-21 14:10:02 -0600

    changed to multipart upload

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
 import os
-
-from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi import FastAPI, Form, UploadFile, File, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from app.validate import validate_and_convert_file
-from createVideo import createVideo
-from getScript import getScript
+from app.createVideo import createVideo
+from app.getScript import getScript
 import shutil
-from fastapi import Response
+import json
 
 app = FastAPI()
 
@@ -23,34 +21,39 @@ app.add_middleware(
 )
 
 class LectureCreate(BaseModel):
-    file: UploadFile
     lecture_title: str
     conciseness: str
     tempo: float
     subtitle: bool = True
 
-@app.post("/createVideo", response_model=FileResponse, summary=
-          """
-            The project create a video presentation from a lecture document (<50 pages). The uploaded files can be in PDF, 
-            Word, or PowerPoint format.
+@app.post("/createVideo", response_class=FileResponse)
+async def create_upload_file(lecture_data_json: str = Form(...), lecture_file: UploadFile = File(...)):
+    """
+        The project create a video presentation from a lecture document (<50 pages). The uploaded files can be in PDF, 
+        Word, or PowerPoint format. Input the parameters as a json string in the lecture_data_json field. The parameters are as follows:\n
 
-            
-            Input Parameters:
+        - **file**: An UploadFile object representing the uploaded file. This file can be a PDF, a Word document, or a PowerPoint presentation. The file size must be between 1 and 5 MB.\n
 
-            file: An UploadFile object representing the uploaded file. This file can be a PDF, a Word document, or a PowerPoint presentation.
-            The file size must be between 1 and 5 MB.
+        - **lecture_title**: A string representing the title of the lecture for context purposes.
 
-            lecture_title: A string representing the title of the lecture for context purposes.
+        - **conciseness**: A string representing the level of conciseness of the lecture. The options are "concise", "medium", and "verbose".
 
-            conciseness: A string representing the level of conciseness of the lecture. The options are "concise", "medium", and "verbose".
+        - **tempo**: A float representing the speed of the video. The options are 0.5, 0.75, 1.0, 1.25, and 1.5, 1.75, and 2.
 
-            tempo: A float representing the speed of the video. The options are 0.5, 0.75, 1.0, 1.25, and 1.5, 1.75, and 2.
-          """)
-async def create_upload_file(lecture_data: LectureCreate):
-    file = lecture_data.file
-    lecture_title = lecture_data.lecture_title
-    conciseness = lecture_data.conciseness
-    tempo = lecture_data.tempo
+        Example parameter input: {"lecture_title": "Introduction to Python", "conciseness": "concise", "tempo": 1.5}
+    """
+    try:
+        # Convert the stringified JSON to a dict
+        item_data = json.loads(lecture_data_json)
+        # Convert the dict to the desired Pydantic model
+        item = LectureCreate(**item_data)
+        params = item.model_dump(exclude={"restaurant_name"})
+        lecture_title = params["lecture_title"]
+        conciseness = params["conciseness"]
+        tempo = params["tempo"]
+    except (json.JSONDecodeError, ValidationError):
+        raise HTTPException(status_code=400, detail="Invalid item data")
+    
     
     temp_dir = "temp_files"
     file_dir = os.path.join(temp_dir, "converted.pdf")
@@ -59,7 +62,7 @@ async def create_upload_file(lecture_data: LectureCreate):
     
     try:
         # Validate and convert the uploaded file to PDF
-        await validate_and_convert_file(file_dir, file)
+        await validate_and_convert_file(file_dir, lecture_file)
         
         # Generate the script for the lecture
         script = getScript(file_dir, lecture_title, conciseness)
